ed04fe45ec14816f30c2307fd3dcdf745c7a5d4a
Neveen97 <neveenaburomman1@gmail.com>
2022-02-02 03:07:33 +0200

    not done yet

--- server.js
+++ server.js
@@ -4,9 +4,14 @@ const express = require('express');
 const jsonData = require("./Movie Data/data.json")
 const app = express();
 const axios = require("axios");
+
 const dotenv = require("dotenv");
 
 dotenv.config();
+
+
+const APIKEY=process.env.APIKEY;
+
 
 
 
@@ -32,9 +37,47 @@ function homeHandlerPage(req,res){
 }
 console.log("hi again");
 
+function trendingHandlerpage(req,res){
+       let trendingmovie=[];
+      axios.get(`https://api.themoviedb.org/3/trending/all/week?api_key=${APIKEY}&language=en-US`)
+           .then(value => {
+         value.data.results.forEach (movie => {
+             let firstMovie =new movieBrief(movie.id, movie.title, movie.release_date, movie.poster_path, movie.overview) 
+
+             trendingmovie.push(firstMovie );
+         });
+         return res.status(200).json(trendingmovie)
+         }).catch(error =>{
+         errorHandler(error,req,res);
+     })
+ }
+ ______________
 
 
 
+
+
+
+
+
+ let topRatedPageHandler = (req, res) => {
+    let topRated = [];
+    axios
+      .get(
+        `https://api.themoviedb.org/3/movie/top_rated?api_key=${APIKEY}&language=en-US&page=1`
+      )
+      .then((value) => {
+        value.data.results.forEach((movie) => {
+          movie = new MoviesLibrary(
+            movie.title,
+            movie.poster_path,
+            movie.overview
+          );
+          topRated.push(movie);
+        });
+        return res.status(200).json(topRated);
+      });
+  };
 
 
 
@@ -45,7 +88,14 @@ function errorHandler(message,req,res){
 
     return res.status(500).send(err);
 }
+function notFoundHandler(req,res){
 
+    return  res.status(404).send("no end point with this name found");
+  }
+
+
+
+app.get('/trending',trendingHandlerpage);
 
 
 app.get('/', homeHandlerPage);
@@ -55,7 +105,7 @@ app.get("/favorite", favoriteHandler);
 
 
 
-
+app.use("*",notFoundHandler);
 
 app.listen(3000, () => {
     console.log(
