7358d9ac5a200c16494cfffbe63f1898dee394d0
Neveen97 <neveenaburomman1@gmail.com>
2022-02-02 16:58:22 +0200

    task12 is done

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 "use strict";
 
+
 const express = require('express');
 const jsonData = require("./Movie Data/data.json")
 const app = express();
@@ -9,6 +10,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 
+const APIKEY=process.env.APIKEY;
 
 
 
@@ -34,27 +36,107 @@ console.log("hi again");
 
 
 
+function trendingHandlerpage(req,res){
+    let trendingmovie=[];
+   axios.get(`https://api.themoviedb.org/3/trending/all/week?api_key=${APIKEY}&language=en-US`)
+        .then(value => {
+      value.data.results.forEach (movie => {
+          let firstMovie =new movieBrief(movie.id, movie.title, movie.release_date, movie.poster_path, movie.overview) 
+
+          trendingmovie.push(firstMovie );
+      });
+
+      return res.status(200).json(trendingmovie)
+      }).catch(error =>{
+      errorHandler(error,req,res);
+  })
+}
+
+function searchHandler(req,res){
+
+    let querySearch=req.query.search;
+    
+    let searchmovie=[];
+
+   axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${APIKEY}&language=en-US&query=${querySearch}`)
+        .then(value => {
+      value.data.results.forEach (movie => {
+          let firstMovie =new movieBrief(movie.id, movie.title, movie.release_date, movie.poster_path, movie.overview) 
+
+          searchmovie.push(firstMovie);
+      });
+
+      return res.status(200).json(searchmovie)
+      }).catch(error =>{
+      errorHandler(error,req,res);
+  })}
 
 
+  function topRatedHandler(req,res){
+    
+    let topRated=[];
 
+   axios.get(`https://api.themoviedb.org/3/movie/top_rated?api_key=${APIKEY}&language=en-US&page=1`)
+        .then(value => {
+      value.data.results.forEach (movie => {
+          let firstMovie =new movieBrief(movie.id, movie.title, movie.release_date, movie.poster_path, movie.overview) 
+
+          topRated.push(firstMovie);
+      });
+
+      return res.status(200).json(topRated)
+      }).catch(error =>{
+      errorHandler(error,req,res);
+  })}
+
+
+  function genreHandler(req,res){
+
+    let generMovie=[];
+
+   axios.get(`https://api.themoviedb.org/3/genre/movie/list?api_key=${APIKEY}&language=en-US`)
+        .then(value => {
+      value.data.results.forEach (movie => {
+          let firstMovie =new movieBrief(movie.id, movie.title, movie.release_date, movie.poster_path, movie.overview) 
+
+          generMovie.push(firstMovie);
+      });
+
+      return res.status(200).json(generMovie)
+      }).catch(error =>{
+      errorHandler(error,req,res);
+
+  })}
 
 
 function errorHandler(message,req,res){
     
-    const err={ status : 500,   message : message.message };
+    const err={ status : 500,  message : message.message };
 
     return res.status(500).send(err);
 }
 
 
+function notFoundHandler(req,res){
 
-app.get('/', homeHandlerPage);
-
-app.get("/favorite", favoriteHandler);
-
-
+    return  res.status(404).send("no end point with this name found");
+  }
 
 
+
+  app.get('/', homeHandlerPage);
+  
+  app.get("/favorite", favoriteHandler);
+  
+  app.get('/trending',trendingHandlerpage);
+
+  app.get('/search',searchHandler);
+
+  app.get('/toprated',topRatedHandler);
+
+  app.get('/genre',genreHandler);
+
+  app.use("*",notFoundHandler);
 
 
 app.listen(3000, () => {
